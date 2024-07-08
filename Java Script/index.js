0170bc33280728cfece1ecdf12a1cc4d8e00cfe5
Utkarsh13124 <utkarshp579@gmail.com>
2024-07-08 18:00:21 +0530

    Start js , complete html , doubt session

---
+++ Java Script/index.js
@@ -0,0 +1,44 @@
+// 68.txt
+console.log('My name is : Utkarsh');
+
+
+
+
+
+
+
+/*
+    named meory location ko hum vaariable bolte hi.
+        let a = 5 , all things are in JS cheat sheet.
+        different  types in same variable , it is dynamic typed language.
+
+    let vs var
+        let ka lifetime codeblock k beech me hi rhega.
+        var ka uuse krke hum global variable bnate hi. 
+        const se fixed value create krte hi.
+
+    Primitive types:
+        string , number , boolean , undefined , null
+        null after clearing some defined value. while undefined me abhi naam variale create kiya  hi but intialise ya define nhi kiya hi
+
+    Operator .. also in js cheat sheet
+
+    Non-Boolean
+        falsy : undefined , null , 0 , false , NaN , ' '
+        truthy : true , 1 , any num , string , char , anything that is not falsy.
+
+    Operator-Precedence
+        if we use brackets , then no need to learn the precedence order.
+        x**y ==> x^y
+    
+    Switch Case : in Cheetsheet
+        writing break condition is important.
+
+    Loop
+        for loop...(let i = 0 ; i < n ; i++)
+        while loop ...()
+        do-while loop ...in cheetsheet
+        for in loop
+        for of loop
+
+*/
