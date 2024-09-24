3eed1bfdb937fddcb846ca822e37ec9731703391
Utkarsh13124 <utkarshp579@gmail.com>
2024-09-24 20:26:51 +0530

    make counter app with react

--- React/demoshopapp/src/App.js
+++ React/demoshopapp/src/App.js
@@ -28,24 +28,47 @@ function App() {
 
 ]
   return (// jsx is html file in javascript
-     <Card>
+     <Card  className='product-item'>
+      {/* <ProductDate date = {props.date} /> */}
           <div> 
-          <ItemDate  day={response[0].itemDay} month={response[0].itemMonth} year={response[0].itemYear}></ItemDate>{/*hum kuch data props ki help se custom component me bhej rhe hi  */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            {/* Class 1 */}
+          {/* <ItemDate  day={response[0].itemDay} month={response[0].itemMonth} year={response[0].itemYear}></ItemDate>{/*hum kuch data props ki help se custom component me bhej rhe hi  */}
           {/* we created this div tag as we follow practice to keep two siblings have same parent , as below , app , item belongs to two different parent --> */}
           <Item name={response[0].itemName}>
             Jai shree Ram. Ye wala conteent props.children se visible hoga.
             </Item>{/* // using custom created Item Element
-        // ye javascriptxml hi */}
+        // ye javascriptxml hi */} 
 
-          <ItemDate  day={response[1].itemDay} month={response[1].itemMonth} year={response[1].itemYear}></ItemDate>
+          {/* <ItemDate  day={response[1].itemDay} month={response[1].itemMonth} year={response[1].itemYear}></ItemDate>
         <Item name={response[1].itemName}></Item>
           
           <ItemDate day={response[2].itemDay} month={response[2].itemMonth} year={response[2].itemYear}></ItemDate>
           <Item name={response[2].itemName} ></Item>
 
-          <ItemDate  day="23" month="June" year="1998"></ItemDate>
-          <Item name={itemName2}></Item>
-          <div className="App">Hello Jee</div>
+          <ItemDate  day="23" month="June" year="1998"></ItemDate> */}
+          {/* <Item name={itemName2}></Item>
+          <div className="App">Hello Jee</div> */}
         </div>
      </Card>
   );
